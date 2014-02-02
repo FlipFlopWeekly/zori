af0b884c63faf38ccb74d848ea0c422c4700fdcd
Thibaud Colas <thibaudcolas@gmail.com>
2014-02-02 01:06:38 +0100

    Add Sass processing task + into watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var path = require('path');
+var sass = require('gulp-ruby-sass');
+var autoprefix = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
 var express = require ('express');
 var lr = require('tiny-lr');
 
@@ -7,6 +10,19 @@ var server = lr();
 var app = express();
 
 var APP_ROOT = __dirname + '/source';
+
+gulp.task('sass', function () {
+  return gulp.src([
+      'source/sass/*.scss',
+      '!source/sass/_*.scss'
+    ])
+    .pipe(sass())
+    .pipe(autoprefix())
+    .pipe(minify({
+      relativeTo: APP_ROOT
+    }))
+    .pipe(gulp.dest('source/assets/css'));
+});
 
 // Statically serves files and adds the LiveReload script.
 gulp.task('serve', function () {
@@ -16,15 +32,17 @@ gulp.task('serve', function () {
 });
 
 // Watches for file changes and reloads browser pages.
-gulp.task('watch', ['serve'], function () {
-
+gulp.task('watch', ['serve', 'sass'], function () {
   server.listen(35729, function (err) {
     if (err) {
       return console.log(err);
     }
 
+    gulp.watch('source/sass/**/*.scss', ['sass']);
+
     gulp.watch([
       'source/index.html',
+      'source/assets/css/*.css',
       'source/js/**/*',
       '!source/js/**/*.spec.js'
     ], function (evt) {
