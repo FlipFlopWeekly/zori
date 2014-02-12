3dbf60ef04e58c887755263b17638fe975f4d597
Thibaud Colas <thibaudcolas@gmail.com>
2014-02-12 08:25:00 +0100

    Finish removing old dependencies

--- source/js/config-require.js
+++ source/js/config-require.js
@@ -8,8 +8,6 @@ define({
   // Here paths are set relative to `/source/js` folder.
   paths: {
     'angular'               : '../vendor/angular/angular',
-    'angular-resource'      : '../vendor/angular-resource/angular-resource',
-    'angular-mocks'         : '../vendor/angular-mocks/angular-mocks',
     'angular-ui-router'     : '../vendor/angular-ui-router/release/angular-ui-router',
     'async'                 : '../vendor/requirejs-plugins/src/async',
     'domReady'              : '../vendor/requirejs-domready/domReady'
@@ -21,8 +19,6 @@ define({
     },
     'angular-ui-router' : {
       'deps': ['angular']
-    },
-    'angular-mocks': ['angular'],
-    'angular-resource': ['angular']
+    }
   }
 });
