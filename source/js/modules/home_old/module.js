677340b056ff97c462184b9b154a93c6ad6b9a10
MonsieurLuge <monsieurluge@gmail.com>
2014-02-18 13:38:34 +0000

    Remove home_old

--- source/js/modules/home_old/module.js
+++
@@ -1,7 +0,0 @@
-/**
- * Attach controllers to this module
- **/
-define(['angular', '../../config', '../../services'], function (ng) {
-  'use strict';
-  return ng.module('app.home', ['app.constants', 'app.services']);
-});
