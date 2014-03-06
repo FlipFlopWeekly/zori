627b280e43860519dc48ab02debfd1193917e91e
Thibaud Colas <thibaudcolas@gmail.com>
2014-03-06 08:14:53 +0100

    Improve zoriLink directive

--- source/js/modules/home/home-directives.js
+++ source/js/modules/home/home-directives.js
@@ -1,11 +1,47 @@
-define(['./module', 'text!./link.html', ], function(directives, linkTpl) {
+define([
+    './module',
+    'jquery',
+    'text!./link.html'
+], function(directives, $, linkTpl) {
     'use strict';
 
     directives.directive('zoriLink', ['$interval',
         function($interval) {
+            function link(scope, element, attrs) {
+                var timeoutId;
+                var $link = $(element).find('a');
+
+                function update() {
+                    var nbClick = scope.link.nbClick;
+                    var height = nbClick * 30 + 30;
+                    var hue = nbClick * 10 + 10;
+
+                    var style = {
+                        height      : height + 'px',
+                        'margin-top': -(height / 2) + 'px',
+                        background  : 'linear-gradient(to bottom, hsl(' + hue + ', 99%, 65%) 50%, hsl(' + hue + ', 99%, 30%) 51%)'
+                    };
+
+                    $link.css(style);
+                }
+
+                scope.$watch(attrs.myCurrentTime, function(value) {
+                    update();
+                });
+
+                element.on('$destroy', function() {
+                    $interval.cancel(timeoutId);
+                });
+
+                timeoutId = $interval(function() {
+                    update();
+                }, 1000);
+            }
+
             return {
                 restrict: 'A',
-                template: linkTpl
+                template: linkTpl,
+                link: link
             };
         }
     ]);
